ce28d480b34c18218c3eb98e37ddc827f67e0185
hordiy <42146903+hordiy@users.noreply.github.com>
2019-07-30 15:44:55 +0300

    Add files via upload

--- HordiyBot.py
+++ HordiyBot.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
+
 import random
 import config
 import telebot
@@ -11,9 +12,7 @@ from bs4 import BeautifulSoup
 import datetime
 import pytz
 import pyowm
-
-
-GREETING = ['здравствуй', 'привет', 'ку', 'здорово']
+from typing import List
 
 bot = telebot.TeleBot(config.TOKEN)
 
@@ -24,9 +23,9 @@ class Weather:
 		self.owm = pyowm.OWM("a53a2754ece8267b620ebf100d39617f", language="ru")
 		self.observation = self.owm.weather_at_place("Kharkiv")
 		self.w = self.observation.get_weather()
-		self.temp = self.w.get_temperature("celsius")["temp"]
+		self.temp: float = self.w.get_temperature("celsius")["temp"]
 	
-	def weather(self, message):
+	def weather(self, message: str) -> str:
 
 		#place = input("City/Country: ")
 		bot.send_message(message.chat.id, "В городе {} сейчас {}".format("Харьков", self.w.get_detailed_status()))
@@ -42,7 +41,7 @@ class Weather:
 # Class for select search 'google' or 'youtube'
 class Search:
 
-	def searchInfo(self, message):
+	def searchInfo(self, message: str) -> str:
 		keyboard = types.InlineKeyboardMarkup()
 		keyboard.row(
 				types.InlineKeyboardButton(text='Перейти на "Google".', url='https://www.google.com/')
@@ -60,7 +59,7 @@ class Currency:
 	    return json.loads(requests.get(config.URL).text)  
 
 	@staticmethod
-	def get_exchange(ccy_key):
+	def get_exchange(ccy_key: int) -> bool:
 		for exc in Currency.load_exchange():
 			if ccy_key == exc['ccy']:
 				return exc
@@ -74,7 +73,7 @@ class Currency:
 				result.append(exc)
 		return result
 
-	def exchange(self, message):
+	def exchange(self, message: str) -> str:
 		keyboard = telebot.types.InlineKeyboardMarkup()
 		keyboard.row(
 				telebot.types.InlineKeyboardButton('USD', callback_data='get-USD'),
@@ -86,23 +85,23 @@ class Currency:
 class Game:
 
 	def __init__(self):
-		self.figures = ["Камень", "Бумага", "Ножницы"]
+		self.figures: List[str] = ["камень", "бумага", "ножницы"]
 
-	def generate_markup(self):
+	def generate_markup(self) -> str:
 	    markup = types.ReplyKeyboardMarkup(row_width=3)
 	    markup.row("Камень", "Бумага", "Ножницы")
 	    return markup
 
-	def generate_figure(self):
+	def generate_figure(self) -> int:
 	    return random.sample(self.figures, 1)
 
-	def define_winner(self, message):
-		answer, reply = message.text, self.generate_figure()[0]
+	def define_winner(self, message: str) -> str:
+		answer, reply = message.text.lower(), self.generate_figure()[0]
 		status = self.figures.index(answer) - self.figures.index(reply)
-		responses = ['Ничья :d', 'Твоя победа :)', 'Твой проигрыш :(']
+		responses: List[str] = ['Ничья :d', 'Твоя победа :)', 'Твой проигрыш :(']
 		bot.send_message(message.chat.id,
-			'HordiyBot: ' + reply + '\nvs \n' + message.from_user.first_name + ': ' +
-			answer + '\n' + responses[status])
+			'HordiyBot: ' + reply.title() + '\nvs \n' + message.from_user.first_name + ': ' +
+			answer.title() + '\n' + responses[status])
 
 # Main class with inheritance of other classes
 class Main(Weather, Currency, Search, Game):
@@ -110,21 +109,22 @@ class Main(Weather, Currency, Search, Game):
 	def __init__(self):
 		Weather.__init__(self)
 		Game.__init__(self)
-		self.now = datetime.datetime.now(pytz.timezone("Europe/Kiev"))
+		self.GREETING: List[str] = ['здравствуй', 'привет', 'ку', 'здорово']
+		self.now: int = datetime.datetime.now(pytz.timezone("Europe/Kiev"))
 
-	def sayHello(self, message):
-		if message.text.lower() in GREETING and 6 <= self.now.hour < 12:
+	def sayHello(self, message: str) -> str:
+		if message.text.lower() in self.GREETING and 6 <= self.now.hour < 12:
 			bot.send_message(message.chat.id, 'Доброе утро, {}'.format(message.from_user.first_name))
-		elif message.text.lower() in GREETING and 12 <= self.now.hour < 17:
+		elif message.text.lower() in self.GREETING and 12 <= self.now.hour < 17:
 			bot.send_message(message.chat.id, 'Добрый день, {}'.format(message.from_user.first_name))
-		elif message.text.lower() in GREETING and 17 <= self.now.hour < 23:
+		elif message.text.lower() in self.GREETING and 17 <= self.now.hour < 23:
 			bot.send_message(message.chat.id, 'Добрый вечер, {}'.format(message.from_user.first_name))
 		else:
 			bot.send_message(message.chat.id, 'Доброй ночи, {}'.format(message.from_user.first_name))			
 
 # BackUp Photo to other group
 @bot.message_handler(content_types=['photo'])
-def handle_docs_photo(message):   
+def handle_docs_photo(message: str) -> str:   
     try:     
         file_info = bot.get_file(message.photo[len(message.photo)-1].file_id)
         downloaded_file = bot.download_file(file_info.file_path)
@@ -136,14 +136,14 @@ def handle_docs_photo(message):
 
 # Main method for text
 @bot.message_handler(content_types=['text'])
-def main(message):
+def main(message: str) -> str:
 	superMain = Main()
 	
 	print("{time}: {first_name} {last_name} ==> {message}".format(first_name=message.from_user.first_name,  
 														   last_name=message.from_user.last_name, 
 														   message=message.text,
 														   time=superMain.now))
-	if message.text.lower() in GREETING:
+	if message.text.lower() in superMain.GREETING:
 		superMain.sayHello(message)
 	elif message.text.lower() == 'курс':
 		superMain.exchange(message)
@@ -155,7 +155,7 @@ def main(message):
 		bot.send_message(message.chat.id,
                      'Выбери одно из трех:',
                      reply_markup=superMain.generate_markup())
-	elif message.text in superMain.figures:
+	elif message.text.lower() in superMain.figures:
 		superMain.define_winner(message)		
 
 # Method for query
@@ -169,7 +169,7 @@ def get_ex_callback(query):
 	    bot.answer_callback_query(query.id)  
 	    send_exchange_result(query.message, query.data[4:])
 
-def send_exchange_result(message, ex_code):  
+def send_exchange_result(message: str, ex_code: int) -> str:  
     bot.send_chat_action(message.chat.id, 'typing')  
     ex = Currency.get_exchange(ex_code)  
     bot.send_message(  
@@ -177,7 +177,7 @@ def send_exchange_result(message, ex_code):
 	parse_mode='HTML'  
     )
 
-def serialize_ex(ex_json, diff=None):  
+def serialize_ex(ex_json, diff=None) -> str:  
 	result = '<b>' + ex_json['base_ccy'] + ' -> ' + ex_json['ccy'] + ':</b>\n\n' + 'Buy: ' + ex_json['buy']  
 	if diff:  
 	    result += ' ' + serialize_exchange_diff(diff['buy_diff']) + '\n' + 'Sell: ' + ex_json['sale'] + ' ' + serialize_exchange_diff(diff['sale_diff']) + '\n'  
@@ -186,7 +186,7 @@ def serialize_ex(ex_json, diff=None):
 	return result
 
 
-def serialize_exchange_diff(diff):  
+def serialize_exchange_diff(diff: int) -> str:  
     result = ''  
     if diff > 0:  
         result = '(' + str(diff) + ' <img draggable="false" data-mce-resize="false" data-mce-placeholder="1" data-wp-emoji="1" class="emoji" alt="<img draggable="false" data-mce-resize="false" data-mce-placeholder="1" data-wp-emoji="1" class="emoji" alt="<img draggable="false" data-mce-resize="false" data-mce-placeholder="1" data-wp-emoji="1" class="emoji" alt="<img draggable="false" data-mce-resize="false" data-mce-placeholder="1" data-wp-emoji="1" class="emoji" alt="<img draggable="false" data-mce-resize="false" data-mce-placeholder="1" data-wp-emoji="1" class="emoji" alt="↗️" src="https://s.w.org/images/core/emoji/2.3/svg/2197.svg">" src="https://s.w.org/images/core/emoji/2.3/svg/2197.svg">" src="https://s.w.org/images/core/emoji/2.3/svg/2197.svg">" src="https://s.w.org/images/core/emoji/72x72/2197.png">" src="https://s.w.org/images/core/emoji/72x72/2197.png">)'  
@@ -195,6 +195,6 @@ def serialize_exchange_diff(diff):
     return result
 
 
-
 if __name__ == '__main__':
-	bot.polling(none_stop=True) 	
+	bot.infinity_polling(True)
+	#bot.polling(none_stop=True) 	
