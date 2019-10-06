1db131a8ed5bd02796e145f58f83d19e7fc5ac7d
hordiy <42146903+hordiy@users.noreply.github.com>
2019-10-06 17:09:43 +0300

    Add files via upload

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-TOKEN = 'Your token'
+TOKEN = '771719555:AAFMH2cSVP27X1mEBtAroFk1E8lyrwynehg'
 TIMEZONE = 'Europe/Kiev'
 TIMEZONE_COMMON_NAME = 'Kiev'
 URL = 'https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5'
